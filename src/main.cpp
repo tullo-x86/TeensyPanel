dbff57921f34dc0737ff8961d7c3e5e6f0348ba9
Daniel Tullemans <tully@be-lumino.us>
2015-02-24 14:47:51 -0600

    Now displaying kriegsman's blur2d() demo

--- src/main.cpp
+++ src/main.cpp
@@ -6,53 +6,41 @@ void draw(int, int);
 const int drawDelayMs = 500;
 uint32_t lastDraw = 0;
 
+const int kBorderWidth = 2;
+
 int main() {
     delay(100);
     pinMode(13, OUTPUT);
     LEDS.addLeds<WS2811, 2, GRB>(Matrix::frameBuffer, NUM_LEDS);
-    LEDS.setBrightness(16);
+    LEDS.setBrightness(64);
     set_max_power_in_volts_and_milliamps(5, 1000);
     set_max_power_indicator_LED(13);
 
     int xOffset = 0;
     while(1) {
-        uint32_t now = millis();
-        if (now > lastDraw + drawDelayMs) {
-            lastDraw = now;
-            draw(xOffset, 0);
-            xOffset += 1;
-            if (xOffset > 28) xOffset = -3;
-        }
+        // Apply some blurring to whatever's already on the matrix
+        // Note that we never actually clear the matrix, we just constantly
+        // blur it repeatedly. Since the blurring is 'lossy', there's
+        // an automatic trend toward black -- by design.
+        uint8_t blurAmount = beatsin8(2,20,255);
+        blur2d(Matrix::frameBuffer, kMatrixWidth, kMatrixHeight, blurAmount);
         
-        blur2d(Matrix::frameBuffer, kMatrixWidth, kMatrixHeight, 32);
-
+        // Use two out-of-sync sine waves
+        uint8_t i = beatsin8( 27, kBorderWidth, kMatrixHeight-kBorderWidth);
+        uint8_t j = beatsin8( 41, kBorderWidth, kMatrixWidth-kBorderWidth);
+        // Also calculate some reflections
+        uint8_t ni = (kMatrixWidth-1)-i;
+        uint8_t nj = (kMatrixWidth-1)-j;
+        // The color of each point shifts over time, each at a different speed.
+        uint16_t ms = millis();
+        Matrix::frameBuffer[XYsafe( i, j)] += CHSV( ms / 11, 200, 255);
+        Matrix::frameBuffer[XYsafe( j, i)] += CHSV( ms / 13, 200, 255);
+        Matrix::frameBuffer[XYsafe(ni,nj)] += CHSV( ms / 17, 200, 255);
+        Matrix::frameBuffer[XYsafe(nj,ni)] += CHSV( ms / 29, 200, 255);
+        Matrix::frameBuffer[XYsafe( i,nj)] += CHSV( ms / 37, 200, 255);
+        Matrix::frameBuffer[XYsafe(ni, j)] += CHSV( ms / 41, 200, 255); 
+        
         show_at_max_brightness_for_power();
         delay_at_max_brightness_for_power(30);
-    }
-}
-
-bool isCellBright(int xCoord, int yCoord, int iteration) {
-    
-    int threshold = (1 << iteration);
-    
-    if (xCoord >= threshold && yCoord >= threshold)
-        return false;
-    else if (xCoord < 0 || yCoord < 0)
-        return false;
-    else if (xCoord == 0 || yCoord == 0)
-        return true;
-    else
-        return isCellBright(xCoord % threshold, yCoord % threshold, iteration - 1);
-}
-
-const int maxIterations = 4;
-
-void draw(int xOffset, int yOffset) {
-    for(int xCoord = 0; xCoord < 16; xCoord++) {
-        for(int yCoord = 0; yCoord < 16; yCoord++) {
-            
-            CRGB color = isCellBright(xCoord + xOffset, yCoord + yOffset, maxIterations) ? 0xFFFFFF : 0x000000;
-            Matrix::frameBuffer[XYsafe(xCoord, yCoord)] = color;
-        }
     }
 }
